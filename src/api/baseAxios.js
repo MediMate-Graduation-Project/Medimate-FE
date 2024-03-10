f193484b026970517b06f9082365b599fb7a059f
anhminh362 <anhminhletran362@gmail.com>
2024-03-10 12:15:59 +0700

    config structure

---
+++ src/api/baseAxios.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+const BASE_URL = import.meta.env.VITE_BASE_URL_API;
+
+const baseAxios = axios.create({
+    baseURL: BASE_URL, 
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+export default baseAxios;
